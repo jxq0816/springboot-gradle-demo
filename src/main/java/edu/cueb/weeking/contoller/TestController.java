a90f9579ff9314f57c8f284ece433c392ddc4ec2
jiangxingqi <jxq0816@126.com>
2017-10-12 23:37:29 +0800

    jiangxingqi:application

--- src/main/java/edu/cueb/weeking/contoller/TestController.java
+++ src/main/java/edu/cueb/weeking/contoller/TestController.java
@@ -7,16 +7,11 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@EnableAutoConfiguration
 public class TestController {
 
     @RequestMapping("/")
     @ResponseBody
     public String home() {
         return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(TestController.class, args);
     }
 }
