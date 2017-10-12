fecdb4bf9e2a1fa984acb54d298f0ff03501b054
jiangxingqi <jxq0816@126.com>
2017-10-13 00:40:15 +0800

    jiangxingqi:包结构更改

--- src/main/java/edu/cueb/weeking/Application.java
+++ src/main/java/edu/cueb/weeking/Application.java
@@ -1,4 +1,4 @@
-package edu.cueb.weeking.contoller;
+package edu.cueb.weeking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
